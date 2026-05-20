2b13be742cf89449ebc0791615cf99e43e6a568c
cdanymar <cdanymar@pm.me>
2026-05-20 23:03:44 +0200

    feat: implement folder tagging

--- Tag-Folder.ps1
+++ Tag-Folder.ps1
@@ -69,3 +69,80 @@ function Write-IniFile($Filename, $Section, $Key, $Value) {
 
     Set-Content -Path $Filename -Value $lines;
 }
+
+function Show-TagDialog($InitialValue) {
+    Add-Type -AssemblyName System.Windows.Forms;
+    Add-Type -AssemblyName System.Drawing;
+
+    $form = [System.Windows.Forms.Form]@{
+        Text = 'Enter Tag';
+        Size = New-Object System.Drawing.Size(320, 125);
+        StartPosition = 'CenterScreen';
+        FormBorderStyle = 'FixedDialog';
+        MaximizeBox = $false;
+        MinimizeBox = $false;
+    };
+
+    $label = [System.Windows.Forms.Label]@{
+        Text = 'Tag:';
+        Location = New-Object System.Drawing.Point(15, 15);
+        AutoSize = $true;
+    };
+
+    $textBox = [System.Windows.Forms.TextBox]@{
+        Location = New-Object System.Drawing.Point(50, 12);
+        Width = 235;
+        Text = $InitialValue;
+    };
+
+    $okButton = [System.Windows.Forms.Button]@{
+        Text = 'OK';
+        Location = New-Object System.Drawing.Point(125, 45);
+        Size = New-Object System.Drawing.Size(75, 25);
+        DialogResult = 'OK';
+    };
+
+    $cancelButton = [System.Windows.Forms.Button]@{
+        Text = 'Cancel';
+        Location = New-Object System.Drawing.Point(210, 45);
+        Size = New-Object System.Drawing.Size(75, 25);
+        DialogResult = 'Cancel';
+    };
+
+    $form.AcceptButton = $okButton;
+    $form.CancelButton = $cancelButton;
+    $form.Controls.AddRange(@($label, $textBox, $okButton, $cancelButton));
+
+    $dialogResult = $form.ShowDialog();
+    $tag = $textBox.Text.Trim();
+    $form.Dispose();
+
+    return [PSCustomObject]@{
+        Result = $dialogResult;
+        Tag = $tag;
+    };
+}
+
+function Set-FolderTag($FolderPath) {
+    $iniPath = Join-Path $FolderPath 'desktop.ini';
+    $section = '{F29F85E0-4FF9-1068-AB91-08002B27B3D9}';
+    $key = 'Prop5';
+
+    $rawValue = Read-IniFile $iniPath $section $key '';
+    $currentTag = if ($rawValue -match '^31,(.*)$') {
+        $Matches[1];
+    }
+    else {
+        '';
+    };
+
+    $response = Show-TagDialog $currentTag;
+    if ($response.Result -eq 'OK') {
+        Write-IniFile $iniPath $section $key "31,$($response.Tag)";
+
+        attrib +h +s $iniPath;
+        attrib +s $FolderPath;
+    }
+}
+
+Set-FolderTag $args[0];
